869eedf1709c76a56f7efbbbf55991a6d0698535
tianyu <yu.tian@dazong.com>
2017-06-14 11:58:06 +0800

    add

--- spring-boot-admin-consul/src/main/java/com/tianyu/SpringBootAdminConsulApplication.java
+++ spring-boot-admin-consul/src/main/java/com/tianyu/SpringBootAdminConsulApplication.java
@@ -4,7 +4,6 @@ import de.codecentric.boot.admin.config.EnableAdminServer;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
-import org.springframework.context.annotation.Configuration;
 
 @EnableAutoConfiguration
 @EnableDiscoveryClient
