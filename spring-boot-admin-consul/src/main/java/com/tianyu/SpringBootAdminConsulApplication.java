036ce2f1e000c89737adf9e0864710a23f1387fb
tianyu <yu.tian@dazong.com>
2017-06-14 11:56:01 +0800

    add build info; ignore consul service monitor and admin-ui monitor

--- spring-boot-admin-consul/src/main/java/com/tianyu/SpringBootAdminConsulApplication.java
+++ spring-boot-admin-consul/src/main/java/com/tianyu/SpringBootAdminConsulApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
 import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 import org.springframework.context.annotation.Configuration;
 
-//@Configuration
 @EnableAutoConfiguration
 @EnableDiscoveryClient
 @EnableAdminServer
