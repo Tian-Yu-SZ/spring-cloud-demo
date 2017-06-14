a5354a45da7b6cd0c9f146392313444df5e9a125
ty <HadesSince2012@gmail.com>
2017-06-14 08:36:04 +0800

    init

---
+++ spring-boot-admin-consul/src/main/java/com/tianyu/SpringBootAdminConsulApplication.java
@@ -0,0 +1,18 @@
+package com.tianyu;
+
+import de.codecentric.boot.admin.config.EnableAdminServer;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.context.annotation.Configuration;
+
+//@Configuration
+@EnableAutoConfiguration
+@EnableDiscoveryClient
+@EnableAdminServer
+public class SpringBootAdminConsulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootAdminConsulApplication.class, args);
+	}
+}
